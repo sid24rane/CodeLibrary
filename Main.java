34eed6ce899120fe022019c32579cca92bf34b39
Siddhesh Rane <siddheshrane24@gmail.com>
2017-09-30 00:35:49 +0530

    added sort mapByValue and printmap

--- Main.java
+++ Main.java
@@ -1,16 +1,20 @@
+package com.company;
+
 import java.io.*;
-import java.util.InputMismatchException;
+import java.util.*;
 
 
-public class Main {
+public class Codechef {
 
 
     public static void main(String[] args) throws IOException {
+
 
         InputStream inputStream = System.in;
         OutputStream outputStream = System.out;
         InputReader in = new InputReader(inputStream);
         OutputWriter out = new OutputWriter(outputStream);
+
 
         int T = in.readInt();
 
@@ -18,11 +22,13 @@ public class Main {
             String str = in.readString();
             out.printLine(str);
         }
-        out.printLine(34,45,11);
-
+        
         out.flush();
         out.close();
     }
+
+
+
 
 
 
@@ -140,6 +146,41 @@ public class Main {
 
     }
 
+    static class MapUtils{
+
+        public static <K, V> void printMap(Map<K, V> map) {
+            for (Map.Entry<K, V> entry : map.entrySet()) {
+                System.out.println("Key : " + entry.getKey()
+                        + " Value : " + entry.getValue());
+            }
+        }
+
+        public static HashMap<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
+
+            // 1. Convert Map to List of Map
+            List<Map.Entry<String, Integer>> list =
+                    new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+
+            // 2. Sort list with Collections.sort(), provide a custom Comparator
+            //    Try switch the o1 o2 position for a different order
+            Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+                public int compare(Map.Entry<String, Integer> o1,
+                                   Map.Entry<String, Integer> o2) {
+                    return (o1.getValue()).compareTo(o2.getValue());
+                }
+            });
+
+            // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+            Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+            for (Map.Entry<String, Integer> entry : list) {
+                sortedMap.put(entry.getKey(), entry.getValue());
+            }
+
+
+            return (HashMap<String, Integer>) sortedMap;
+        }
+
+    }
     static class IOUtils {
         public static int[] readIntArray(InputReader in, int size) {
             int[] array = new int[size];
@@ -234,4 +275,3 @@ public class Main {
     }
 
 }
-
